b7325967aedd3f07af0bdce86eb0f6bfb79b4445
XixiXixi <alexis.bert1412@gmail.com>
2025-10-15 15:51:16 +0200

    Revert "Ajout d'une fonctionnalité (toggleTask)"

--- gitcidevB-main/eval-git-ci-js-base/lib/tasks.js
+++ gitcidevB-main/eval-git-ci-js-base/lib/tasks.js
@@ -14,17 +14,4 @@ function reset() {
   nextId = 1;
 }
 
-function addTask(id, nom, done = false) {
-  const task = { id, nom, done};
-  tasks.push(task);
-  return task;
-}
-
-function toggleTask(id) {
-  const task = tasks.find(tache => tache.id === id);
-  if (task) {
-    task.done = true;
-  }
-}
-
-module.exports = { getTasks, reset, addTask, toggleTask };
+module.exports = { getTasks, reset };
